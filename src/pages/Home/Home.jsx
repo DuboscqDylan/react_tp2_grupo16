8e87e8e5f5931f9422b26719bfd46fdc0ab41c11
LucasGabrielCerda <80951433+LucasGabrielCerda@users.noreply.github.com>
2026-04-30 13:58:19 -0300

    Revert "Sistema de Favoritos"

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,30 +1,34 @@
 import { useState, useEffect } from "react";
-import { ListSongs } from "../../components/ListSongs/ListSongs";
-import { useFavorites } from "../../contexts/FavoritesContext";
+import { ListSongs } from '../../components/ListSongs/ListSongs';
+import { Header } from "../../components/Header/Header";
 
-const url = new URL("https://69ed5ad4af4ff533142bb90c.mockapi.io/song");
+const url = new URL('https://69ed5ad4af4ff533142bb90c.mockapi.io/song');
 
 export const Home = () => {
-  const [songs, setSongs] = useState([]);
-  const { toggleFavorite, isFavorite } = useFavorites();
+    const [songs, setSongs] = useState([]);
 
-  useEffect(() => {
-    fetch(url)
-      .then((res) => {
-        if (!res.ok) throw new Error("Error en la request");
-        return res.json();
-      })
-      .then((data) => setSongs(data))
-      .catch((err) => console.error(err));
-  }, []);
+    useEffect(() => {
+        fetch(url)
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error("Error en la request");
+                }
+                return res.json();
+            })
+            .then(data => {
+                setSongs(data);
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    }, []);
 
-  return (
-    <div>
-      <ListSongs
-        list={songs}
-        isFavorite={isFavorite}
-        onToggleFavorite={toggleFavorite}
-      />
-    </div>
-  );
-};
+
+    return (
+        <div>
+            <ListSongs
+                list={songs}
+            />
+        </div>
+    )
+}
