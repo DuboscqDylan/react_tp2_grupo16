60ae134de53e39f8d67212ff67f10d3ce52aa654
DuboscqDylan <dylan.duboscq@est.fi.uncoma.edu.ar>
2026-05-07 16:31:43 -0300

    revisado el header, darkTheme llevado a hook useTheme

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,34 +2,19 @@ import { useEffect, useState } from "react";
 import { DarkModeToggle } from "../DarkModeToggle/DarkModeToggle";
 import { NavBar } from "../NavBar/NavBar";
 import { LanguageSelector } from "../LanguageSelector/LanguageSelector";
+import { useTheme } from "../../hooks/useTheme";
 
 export const Header = () => {
 
-    const [darkMode, setDarkMode] = useState(() => {
-        return localStorage.getItem("theme") === "dark";
-    });
-
-    useEffect(() => {
-        const root = document.documentElement;
-
-        if (darkMode) {
-            root.classList.add("dark");
-        } else {
-            root.classList.remove("dark");
-        }
-
-        localStorage.setItem("theme", darkMode ? "dark" : "light");
-    }, [darkMode]);
+    const { darkMode, setDarkMode } = useTheme();
 
     return (
-        <div>
-            <header className="fixed top-0 left-0 w-full z-50 flex items-center justify-between px-7 py-6 border border-[var(--color-border)] bg-[var(--color-bg)] text-[var(--color-text)]">
-                <NavBar></NavBar>
+            <header className="fixed top-0 left-0 w-full z-50 flex items-center justify-between px-7 py-6 border-b border-[var(--color-border)] bg-[var(--color-bg)] text-[var(--color-text)]">
+                <NavBar />
                 <div className="flex items-center gap-3">
                     <LanguageSelector />
                     <DarkModeToggle darkMode={darkMode} setDarkMode={setDarkMode} />
                 </div>
             </header>
-        </div>
     )
 }
